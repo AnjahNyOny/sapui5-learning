799b2b17a5412302dc0a8b6ffbcededb7d36a5b4
AnjahNyOny <110890442+AnjahNyOny@users.noreply.github.com>
2025-09-16 21:31:47 -0400

    feat(step-09): Component configuration

---
+++ walkthrough-notes/step-09-component-configuration.md
@@ -0,0 +1,10 @@
+# Step 9 : Component configuration
+_ Date : 16 September 20:54
+_ Goal : Creating an input field and a description displaying the value of the input field
+_ What I did : 
+            _ Created a component that manages the display of the app view
+            _ Deleted the onInit function in the controller
+            _ In the index.js, created a component container instead of the view
+
+_ What I learned : 
+            _ How to use a controller
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,31 +1,12 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast",
-   "sap/ui/model/json/JSONModel",
-   "sap/ui/model/resource/ResourceModel"
-], (Controller, MessageToast, JSONModel, ResourceModel) => {
+   "sap/m/MessageToast"
+], (Controller, MessageToast) => {
    "use strict";
 
    return Controller.extend("ui5.walkthrough.controller.App", {
-      onInit() {
-         // set data model on view
-         const oData = {
-            recipient: {
-               name: "Anarana"
-            }
-         };
-         const oModel = new JSONModel (oData);
-         this.getView().setModel(oModel);
-
-         // set i18n on model
-         const i18nModel = new ResourceModel({
-            bundleName: "ui5.walkthrough.i18n.i18n"
-         });
-         this.getView().setModel(i18nModel, "i18n")
-      },
-
-      onShowText() {
-         // read message from i18n model
+      onShowHello() {
+         // read msg from i18n model
          const oBundle = this.getView().getModel("i18n").getResourceBundle();
          const sRecipient = this.getView().getModel().getProperty("/recipient/name");
          const sMsg = oBundle.getText("helloMsg", [sRecipient]);
@@ -33,5 +14,5 @@ sap.ui.define([
          // show message
          MessageToast.show(sMsg);
       }
-   })
+   });
 });
